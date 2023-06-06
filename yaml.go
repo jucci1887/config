6e0b2fe879c69d239a756012a5dd1da3cb83f5b6
jucci1887 <jucci1887@gmail.com>
2023-06-06 20:53:22 +0800

    edit

--- yaml.go
+++ yaml.go
@@ -1,14 +1,15 @@
 /*
- Author: Kernel.Huang
- Mail: kernelman79@gmail.com
- File: config.go
- Date: 8/9/22 1:57 PM
+Author: Kernel.Huang
+Mail: kernelman79@gmail.com
+File: config.go
+Date: 8/9/22 1:57 PM
 */
 package config
 
 import (
 	"github.com/jucci1887/common"
 	"github.com/jucci1887/system"
+	"gopkg.in/yaml.v3"
 	"log"
 )
 
@@ -29,10 +30,11 @@ func (yc *YamlConfig) NewYaml(dirname string, filename string) *YamlConfig {
 
 /*
 Example:
+
 	var structured structuredConfig
 	config.Yaml.NewYaml("config", "config.toml").Parse(&structured)
 */
-func (yc *YamlConfig) Parse(structured any) any {
+func (yc *YamlConfig) Parse(structured interface{}) interface{} {
 	err := yaml.Unmarshal(yc.cfg, structured)
 	if err != nil {
 		log.Println("Parse yaml file error: ", err)
